1638a88ba074afd65f3937efb5a1dcf9336a0137
cherniyk <cherniykvv@gmail.com>
2025-07-31 11:09:40 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,16 @@
+export const menu = [
+  {
+    category: "Сніданки",
+    items: [
+      { name: "Яєчня з беконом", description: "2 яйця, бекон, тост", price: 95 },
+      { name: "Омлет овочевий", description: "З перцем, зеленню і сиром", price: 110 }
+    ]
+  },
+  {
+    category: "Напої",
+    items: [
+      { name: "Кава", description: "Свіжозварена", price: 45 },
+      { name: "Чай", description: "Чорний або зелений", price: 35 }
+    ]
+  }
+];
